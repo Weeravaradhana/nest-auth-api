e124f9d3da575d3b33fb4f4f841823412d0176ba
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-05-31 13:33:46 +0530

    feat(auth): expose public login endpoint with access and refresh token response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 import { RegisterDto } from "./dto/register.dto";
 import { AuthService } from "./auth.service";
 import { VerifyOtpDto } from "./dto/verify-otp.dto";
+import { LoginDto } from "./dto/login.dto";
 
 @Controller()
 export class AuthController {
@@ -23,5 +24,11 @@ export class AuthController {
   @UsePipes(new ValidationPipe({ whitelist: true }))
   async verifyOtp(@Body() dto: VerifyOtpDto) {
     return this.authService.verifyOtp(dto);
+  }
+
+  @Post("login")
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
   }
 }
