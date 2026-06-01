03c1a5e87330ed9346b352b9d39c245a51ed537f
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-01 12:38:37 +0530

    feat(auth): implement JWT auth guard with Redis token blacklist validation

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,22 +1,33 @@
 import {
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import Redis from "ioredis";
+import express from "express";
 
 
 @Injectable()
-export class JwrAuthGuard extends AuthGuard("jwt") {
-
-  canActivate(context: ExecutionContext){
-    return super.canActivate(context)
+export class JwtAuthGuard extends AuthGuard("jwt") {
+  constructor(@Inject("REDIS_CLIENT") private readonly redis: Redis) {
+    super();
   }
 
-  handleRequest(err:any, user:any, info:any){
-    if (err || !user){
-      throw err || new UnauthorizedException('Access denied.Invalid or missing token.')
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request:express.Request = context.switchToHttp().getRequest();
+    const accessToken = request.headers.authorization?.split(' ')[1];
+
+    if(accessToken){
+      const isBlackList = await this.redis.get(`blacklist:${accessToken}`);
+      if (isBlackList){
+        throw new UnauthorizedException(
+          "Access denied. This token has been revoked via logout.",
+        );
+      }
     }
-    return user;
+    return await super.canActivate(context) as boolean
   }
+
 }
